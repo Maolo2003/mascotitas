c2e06ae87f4932f0948feba1f1c3f1261a55a540
Albert Mota <albamota@soy.sena.edu.co>
2023-08-01 16:28:40 -0500

    Se creó la interfaz de login y se configuraron las dependencias, además, se actualizó el build.gradle a la versión 19, se agregó el .env

--- lib/app/config/themes/theme.dart
+++ lib/app/config/themes/theme.dart
@@ -1,15 +1,16 @@
 //Se crea la clase AppTheme que contiene el tema de la aplicación
 import 'package:flutter/material.dart';
 
-const Color _customColor = Color(0xFF027373);
+const Color _customColor = Color(0xFF8F82B7);
 
 // Seleccionar los colores de su aplicación NO UTILIZAR LOS COLORES DE EJEMPLO
 const List<Color> _colorTheme=[
   _customColor,
-  Color(0xFF0D8C7F),
-  Color(0xFF65A69F),
-  Color(0xFF9BBFBC),
-  Color(0xFFF0F1F2),
+  Color(0xFF8F82B7),
+  Color(0xFFFFFFFF),
+  Color(0xFF000000),
+  Color(0xFFD2D1D7),
+  Color(0xFF8F82B7),
 ];
 
 class AppTheme{
--- lib/app/presentation/views/login/login_view.dart
+++ lib/app/presentation/views/login/login_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sekerme_ecommerce/app/presentation/widgets/form_text_field.dart';
+import 'package:sekerme_ecommerce/app/presentation/widgets/my_button_form.dart';
+import '../../../config/themes/theme.dart';
+
 
 class LoginView extends StatelessWidget {
 
@@ -16,19 +20,56 @@ class LoginView extends StatelessWidget {
             width: double.infinity,
             padding: const EdgeInsets.all(8.0),
             child: Column(
-              children: [
-                //Logo
-                SvgPicture.asset('assets/images/MyMarca.svg',
-                  colorFilter:  ColorFilter.mode(
-                      Theme.of(context).colorScheme.primary, BlendMode.srcIn
-                  ),
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [SvgPicture.asset('assets/images/logo_mascotitas.svg',
+                colorFilter:  ColorFilter.mode(
+                    Theme.of(context).colorScheme.primary, BlendMode.srcIn
                 ),
 
+              ),
 
-
-                //Form
-                //Button
-                //TextButton
+                Text("Login",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                ),
+                const SizedBox(height: 20,),
+                const MyFormTextField(
+                  labelText: 'Email',
+                  hintText: 'Enter your email address...',
+                  textInputType: TextInputType.emailAddress,
+                  obscureText: false,
+                  suffixIcon: false,
+                ),
+                const SizedBox(height: 20,),
+                const MyFormTextField(
+                  labelText: 'Password',
+                  hintText: 'Enter your password...',
+                  textInputType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  suffixIcon: true,
+                ),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 1.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                            onPressed: (){},
+                            child: Text('Did you forget your password?',
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.primary,
+                              ),
+                            ),
+                        )
+                      ],
+                    ),
+                ),
+                const SizedBox(height: 20,),
+                MyButtonForm(
+                  text: 'Login',
+                  onTab: (){},
+                )
               ],
             ),
           ),
